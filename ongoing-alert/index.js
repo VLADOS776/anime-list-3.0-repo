53e828cc90d8bfe5f7a4f0b246fce5676a01b1f3
VLADOS776 <kurtukovvlad@gmail.com>
2017-11-01 15:08:42 +0300

    Новая версия "Ongoing Alert"

--- ongoing-alert/index.js
+++ ongoing-alert/index.js
@@ -1,10 +1,15 @@
+const changelog = {
+    '1.1.0': 'Новые серии показываются не только при старте, но и при переходе на стартовую страницу.'
+}
+
 module.exports = function(Plugin) {
     const config = {
         id: 'ongoing-alert',
         name: "Ongoing Alert",
         description: "При старте программы проверяет наличие новых эпизодов и выводит уведомление",
         author: "VLADOS776",
-        version: "1.0.0"
+        version: "1.1.0",
+        changelog: changelog
     }
     
     let Vue;
@@ -13,31 +18,38 @@ module.exports = function(Plugin) {
         init: function(opt) {
             
             setTimeout(function() {
-                let now = new Date();
-
-                let ongoings = opt.app.allAnime.filter(item => item.next_episode_at);
-                let alertText = `<div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Новые эпизоды:</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>`;
-                
-                let count = 0;
-
-                ongoings.forEach(item => {
-                    let nextEp = new Date(item.next_episode_at);
-
-                    if (nextEp - now < 0) {
-                        alertText += `<div>${item.russian || item.name} - ${item.availableEp + 1} эп.</div>`;
-                        count++;
-                        
-                        console.log(item);
-                    }
-                });
-                
-                alertText += `</div>`;
-                
-                if (count > 0)
-                    jQuery('.start').prepend(alertText);
+                showAlert(opt.app.allAnime);
             }, 300)
+        },
+        onEvent: function(event) {
+            if (event.type === 'openPage' && event.page === 'start') {
+                showAlert(event.vue.app.allAnime);
+            }
         }
     })
+    
+    function showAlert(allAnime) {
+        let now = new Date();
+
+        let ongoings = allAnime.filter(item => item.next_episode_at);
+        let alertText = `<div class="alert alert-success alert-dismissible fade show" role="alert"><strong>Новые эпизоды:</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>`;
+
+        let count = 0;
+
+        ongoings.forEach(item => {
+            let nextEp = new Date(item.next_episode_at);
+
+            if (nextEp - now < 0) {
+                alertText += `<div>${item.russian || item.name} - ${item.availableEp + 1} эп.</div>`;
+                count++;
+            }
+        });
+
+        alertText += `</div>`;
+
+        if (count > 0)
+            jQuery('.start').prepend(alertText);
+    }
 }
